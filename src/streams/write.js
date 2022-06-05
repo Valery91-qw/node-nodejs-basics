c0d26f5f76973f3ab9eea9d5cfab804be87da626
Valery91-qw <vakriv91@gmail.com>
2022-06-05 20:28:02 +0300

    feat: implement logic in write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,15 @@
+import {createWriteStream} from "fs";
+import {dirname, join} from "path";
+import {fileURLToPath} from "url";
+
+const pathToReadFile = join(dirname(fileURLToPath(import.meta.url)), "files", "fileToWrite.txt")
+
 export const write = async () => {
-    // Write your code here 
+  const writeStream = createWriteStream(pathToReadFile, {encoding: 'utf8'})
+  process.stdin.on('data', (data) => {
+    writeStream.write(data);
+    process.exit();
+  })
 };
+
+write()
